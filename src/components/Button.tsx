ffc5639a9607d7dc9aba8c7f362ceb57d0b94146
Brandon Stone <42560473+KidLiberty@users.noreply.github.com>
2025-02-05 11:22:47 -0500

    Added timestamps, formatted chatroom

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,10 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 
 const ButtonStyles = {
-  sm: 'p-2 text-xs font-bold bg-emerald-400 cursor-pointer hover:bg-emerald-400/80 active:scale-95 transition-all ',
-  md: 'p-4 px-5 font-bold bg-emerald-400 cursor-pointer hover:bg-emerald-400/80 active:scale-95 transition-all',
-  lg: 'p-5 px-6 text-xl font-bold bg-emerald-400 cursor-pointer hover:bg-emerald-400/80 active:scale-95 transition-all',
-  xl: 'p-6 px-8 text-2xl font-bold bg-emerald-400 cursor-pointer hover:bg-emerald-400/80 active:scale-95 transition-all'
+  sm: 'p-2 text-xs font-bold bg-primary cursor-pointer hover:bg-primary/80 active:scale-95 transition-all ',
+  md: 'p-4 px-5 font-bold bg-primary cursor-pointer hover:bg-primary/80 active:scale-95 transition-all',
+  lg: 'p-5 px-6 text-xl font-bold bg-primary cursor-pointer hover:bg-primary/80 active:scale-95 transition-all',
+  xl: 'p-6 px-8 text-2xl font-bold bg-primary cursor-pointer hover:bg-primary/80 active:scale-95 transition-all'
 }
 
 type ButtonProps<T extends ElementType = 'button'> = ComponentPropsWithoutRef<T> & {
--- src/features/Chat/components/ChatRoom.tsx
+++ src/features/Chat/components/ChatRoom.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useRef, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { collection, addDoc, onSnapshot, orderBy, query, serverTimestamp, doc, getDoc } from 'firebase/firestore'
+import { format } from 'date-fns'
 
 import { db, auth, User } from '../../../config/Firebase'
 import { useUserContext } from '../../../hooks/useUserContext'
 
 import Input from '../../../components/Input'
 import Button from '../../../components/Button'
-import { ChevronLeft } from 'lucide-react'
+import { ArrowUp, ChevronLeft } from 'lucide-react'
 
 type Message = {
   id: string
@@ -26,7 +27,7 @@ export default function ChatRoom() {
 
   // Scroll to the end of a div upon sending a message
   useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
   }, [messages])
 
   // Get current messages from particular chatroom
@@ -81,11 +82,61 @@ export default function ChatRoom() {
     fetchOtherUser()
   }, [chatroomId, user])
 
+  useEffect(() => {
+    if (!chatroomId) return
+
+    const messagesRef = collection(db, 'chatrooms', chatroomId, 'messages')
+
+    const q = query(messagesRef, orderBy('timestamp', 'asc'))
+
+    // Subscribe to Firestore updates
+    const unsubscribe = onSnapshot(q, snapshot => {
+      const snapshotMessages = snapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data(),
+        // This needs to be converted from FS timestamp to Date
+        timestamp: doc.data().timestamp ? doc.data().timestamp.toDate() : null
+      }))
+      setMessages(snapshotMessages as Message[])
+    })
+
+    return () => unsubscribe()
+  }, [chatroomId])
+
+  const formatTimestamp = (timestamp: Date) => {
+    if (!timestamp) return ''
+
+    const messageDate = new Date(timestamp)
+    const now = new Date()
+
+    const isToday = messageDate.toDateString() === now.toDateString()
+    const yesterday = new Date()
+    yesterday.setDate(yesterday.getDate() - 1)
+    const isYesterday = messageDate.toDateString() === yesterday.toDateString()
+
+    const timeFormat = format(messageDate, 'h:mm a') // e.g., '11:10 AM'
+
+    if (isToday) return `Today at ${timeFormat}`
+    if (isYesterday) return `Yesterday at ${timeFormat}`
+
+    // Return format as --> '2/2/2024 at 7:00PM'
+    return format(messageDate, "M/d/yyyy 'at' h:mm a")
+  }
+
+  // Use enter key to send message
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === 'Enter' && newMessage.trim() !== '') {
+      sendMessage()
+      setNewMessage('')
+      event.preventDefault()
+    }
+  }
+
   return (
     <div className='h-screen flex flex-col justify-between'>
       <div className='flex justify-between py-4 p-2 shadow-md'>
         <Link to='/dashboard' >
-          <ChevronLeft />
+          <ChevronLeft className='text-center text-primary hover:text-primary/90 active:scale-90' />
         </Link>
         <h2 className='font-bold'>Chatroom</h2>
         <div />
@@ -93,9 +144,13 @@ export default function ChatRoom() {
 
       <div className='flex-1 p-2 overflow-x-scroll'>
         {messages.map(message => (
-          <p key={message.id}>
-            <strong style={{ color: message.senderId === auth.currentUser?.uid ? 'green' : 'black' }}>{message.senderId === auth.currentUser?.uid ? user?.displayName : otherUser?.displayName}</strong>: {message.text}
-          </p>
+          <>
+            <div key={message.id}>
+              <strong className={`${message.senderId === auth.currentUser?.uid ? 'text-primary' : 'text-black'}`}>{message.senderId === auth.currentUser?.uid ? user?.displayName : otherUser?.displayName}</strong>
+              <span className='pl-2 text-sm text-gray-400'>{message.timestamp ? formatTimestamp(message.timestamp) : ''}</span>
+            </div>
+            <p>{message.text}</p>
+          </>
         ))}
 
         <div ref={messagesEndRef} />
@@ -103,8 +158,17 @@ export default function ChatRoom() {
 
 
       <div className='flex items-center'>
-        <Input className='flex-1' value={newMessage} onChange={e => setNewMessage(e.target.value)} placeholder={`Message @${otherUser?.displayName}`} />
-        <Button className='py-3' onClick={sendMessage}>Send</Button>
+        <div className='w-full flex justify-center items-center m-1 bg-slate-50  rounded-lg shadow-lg'>
+          <Input
+            className='h-full flex-1 mx-2 shadow-none bg-slate-50'
+            value={newMessage}
+            onChange={e => setNewMessage(e.target.value)} placeholder={`Message @${otherUser?.displayName}`}
+            onKeyDown={handleKeyDown}
+          />
+          <Button className='h-full flex items-center justify-center' pill onClick={sendMessage}>
+            <ArrowUp />
+          </Button>
+        </div>
       </div>
     </div>
   )
