1751b99902572d2cdf0890ced5a6bc799bb53db4
Brandon Stone <42560473+KidLiberty@users.noreply.github.com>
2025-02-12 22:58:25 -0500

    Huge styling changes, NotFound route, darkmode persistance

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,7 +3,6 @@ import { UserSearch } from '../features/UserSearch/components/UserSearch'
 import { Header } from '../components/ui/Header'
 
 export default function Dashboard() {
-  // const { chatrooms } = useChatroomsContext()
 
   return (
     <div className='h-screen flex flex-col justify-center items-center p-2'>
